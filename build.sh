747680baf0c79dcf7abee05a779c41043628797a
GOKULTM <104120655+GOKULTM@users.noreply.github.com>
2025-03-16 12:41:06 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 pip install --upgrade pip setuptools wheel
-curl https://sh.rustup.rs -sSf | sh -s -- -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
+
 pip install -r requirements.txt
